8a8acb10c4b80e687cf130c1c151a2ac73bd06b8
Wuor Bhang <uhuribhang211@gmail.com>
2024-11-21 11:23:42 -0800

    new changes to users and wallets integrations

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
