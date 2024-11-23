2b0e1ba5302f8b83019ebd600fb9d162c71c552b
tufstraka <keithkadima@gmail.com>
2024-11-23 15:06:41 +0300

    update models

--- backend(off-chain)/models/project.js
+++ backend(off-chain)/models/project.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  canisterId: { type: String, required: true},
   category: {
     type: String,
     enum: ['tech', 'sustainability', 'health', 'education', 'finance', 'gaming'], // Added 'gaming'
     required: true,
   },
+  stage: { type: String, required: true},
   targetAmount: { type: Number, required: true },
+  teamSize: { type: Number, required: true },
   raisedAmount: { type: Number, default: 0 },
   builder: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   investors: [{
@@ -19,6 +22,7 @@ const projectSchema = new mongoose.Schema({
   backers: { type: Number, default: 0 }, 
   daysLeft: { type: Number, default: 0 }, 
   progress: { type: Number, default: 0 }, 
+  timeline: { type: String, required: true },
   tags: [{ type: String }],
   status: { type: String, enum: ['active', 'funded', 'closed'], default: 'active' },
   createdAt: { type: Date, default: Date.now },
--- backend(off-chain)/models/user.js
+++ backend(off-chain)/models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  fullName: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['builder', 'investor'], required: true },
+  linkedinUrl: { type: String, required: true },
+  experience: { type: String, required: true },
+  userType: { type: String, enum: ['builder', 'investor'], required: true },
   walletAddress: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
